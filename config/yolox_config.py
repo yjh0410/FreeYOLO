0948875a27d9ae6bfba17339f0c17a626d96f3ad
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-04-24 12:27:27 +0800

    update ota

--- config/yolox_config.py
+++ config/yolox_config.py
@@ -4,9 +4,9 @@
 yolox_config = {
     'yolox_s': {
         # input
-        'train_size': 800,
+        'train_size': 640,
         'test_size': 640,
-        'random_size': [448, 480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800],
+        'random_size': [320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640], # [448, 480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800],
         'mosaic': True,
         'mixup': True,
         'format': 'RGB',
