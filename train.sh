f0f5f2b92160e64d462ce1eea21e32ea14544291
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-04-29 12:59:47 +0800

    train yolox_n on coco

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolox_s \
+        -v yolox_n \
         -m sim_ota \
         --ema \
         --eval_epoch 10
