6bafc3254ff251f3e593ab8ab11834c7958e9500
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-05-08 18:09:01 +0800

    modify SimOTA

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ python train.py \
         --cuda \
         -d voc \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolox_t \
+        -v yolox_s \
         -m sim_ota \
         --ema \
         --fp16 \
