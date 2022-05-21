cc5e18c48c8ddb036f34da7116d68c840d05dd81
yjh0410 <1394571815@qq.com>
2022-05-21 08:29:24 +0800

    update yolo_anchor

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 python train.py \
         --cuda \
-        -d voc \
+        -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolo_anchor \
+        -v yolo_free \
         --ema \
         --fp16 \
         --eval_epoch 10
