a7ae6f4c537c43488e3eb75dcfa5c0a8841fece5
yjh0410 <1394571815@qq.com>
2022-05-27 18:48:57 +0800

    update yolof

--- train.sh
+++ train.sh
@@ -6,5 +6,5 @@ python train.py \
         --ema \
         --fp16 \
         --eval_epoch 10 \
-        --start_epoch 141 \
+        --start_epoch 140 \
         --resume weights/coco/yolo_anchor/yolo_anchor_epoch_141_39.97.pth
