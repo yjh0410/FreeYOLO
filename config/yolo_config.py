f883933ea7b4fce089e49378c962a6ca21709758
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-11 13:29:01 +0800

    use larger input size from 320 to 800

--- config/yolo_config.py
+++ config/yolo_config.py
@@ -4,9 +4,12 @@
 yolo_config = {
     'yolo_free': {
         # input
-        'train_size': 640,
+        'train_size': 800,
         'test_size': 608,
-        'random_size': [320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640],
+        'random_size': [320, 352, 384, 416,
+                        448, 480, 512, 544,
+                        576, 608, 640, 672,
+                        704, 736, 768, 800],
         'mosaic_prob': 0.5,
         'mixup_prob': 0.5,
         'format': 'RGB',
