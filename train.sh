98ac4258178cb42a175775d7bea3cbe7aed46bb2
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-07-10 14:47:20 +0800

    resume yolo_free_v2

--- train.sh
+++ train.sh
@@ -5,4 +5,5 @@ python train.py \
         -v yolo_free_v2 \
         --ema \
         --fp16 \
-        --eval_epoch 10
+        --eval_epoch 10 \
+        --resume weights/coco/yolo_free_v2/yolo_free_v2_epoch_51_37.24.pth
