c1b2c481f5d6ac0152f06d5d19bc1894eb4624c9
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-07-14 11:02:47 +0800

    add ELAN Backbone & ELAN PaFPN of YOLOv7

---
+++ models/backbone/elannet.py
@@ -0,0 +1,206 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+model_urls = {
+    "elannet": "",
+}
+
+
+# Basic conv layer
+class Conv(nn.Module):
+    def __init__(self, 
+                 c1,                   # in channels
+                 c2,                   # out channels 
+                 k=1,                  # kernel size 
+                 p=0,                  # padding
+                 s=1,                  # padding
+                 d=1,                  # dilation
+                 act=True,             # activation
+                 depthwise=False):
+        super(Conv, self).__init__()
+        convs = []
+        if depthwise:
+            # depthwise conv
+            convs.append(nn.Conv2d(c1, c1, kernel_size=k, stride=s, padding=p, dilation=d, groups=c1, bias=False))
+            convs.append(nn.BatchNorm2d(c1))
+            if act:
+                convs.append(nn.SiLU(inplace=True))
+
+            # pointwise conv
+            convs.append(nn.Conv2d(c1, c2, kernel_size=1, stride=s, padding=0, dilation=d, groups=1, bias=False))
+            convs.append(nn.BatchNorm2d(c2))
+            if act:
+                convs.append(nn.SiLU(inplace=True))
+
+        else:
+            convs.append(nn.Conv2d(c1, c2, kernel_size=k, stride=s, padding=p, dilation=d, groups=1, bias=False))
+            convs.append(nn.BatchNorm2d(c2))
+            if act:
+                convs.append(nn.SiLU(inplace=True))
+            
+        self.convs = nn.Sequential(*convs)
+
+
+    def forward(self, x):
+        return self.convs(x)
+
+
+class ELANBlock(nn.Module):
+    """
+    ELAN BLock of YOLOv7's backbone
+    """
+    def __init__(self, in_dim, out_dim, expand_ratio=0.5, depthwise=False):
+        super(ELANBlock, self).__init__()
+        inter_dim = int(in_dim * expand_ratio)
+        self.cv1 = Conv(in_dim, inter_dim, k=1)
+        self.cv2 = Conv(in_dim, inter_dim, k=1)
+        self.cv3 = nn.Sequential(
+            Conv(inter_dim, inter_dim, k=3, p=1, depthwise=depthwise),
+            Conv(inter_dim, inter_dim, k=3, p=1, depthwise=depthwise)
+        )
+        self.cv4 = nn.Sequential(
+            Conv(inter_dim, inter_dim, k=3, p=1, depthwise=depthwise),
+            Conv(inter_dim, inter_dim, k=3, p=1, depthwise=depthwise)
+        )
+
+        assert inter_dim*4 == out_dim
+
+        self.out = Conv(inter_dim*4, out_dim, k=1)
+
+
+
+    def forward(self, x):
+        """
+        Input:
+            x: [B, C, H, W]
+        Output:
+            out: [B, 2C, H, W]
+        """
+        x1 = self.cv1(x)
+        x2 = self.cv2(x)
+        x3 = self.cv3(x2)
+        x4 = self.cv4(x3)
+
+        # [B, C, H, W] -> [B, 2C, H, W]
+        out = self.out(torch.cat([x1, x2, x3, x4], dim=1))
+
+        return out
+
+
+class DownSample(nn.Module):
+    def __init__(self, in_dim):
+        super().__init__()
+        inter_dim = in_dim // 2
+        self.mp = nn.MaxPool2d((2, 2), 2)
+        self.cv1 = Conv(in_dim, inter_dim, k=1)
+        self.cv2 = nn.Sequential(
+            Conv(in_dim, inter_dim, k=1),
+            Conv(inter_dim, inter_dim, k=3, p=1, s=2)
+        )
+
+    def forward(self, x):
+        """
+        Input:
+            x: [B, C, H, W]
+        Output:
+            out: [B, C, H//2, W//2]
+        """
+        # [B, C, H, W] -> [B, C//2, H//2, W//2]
+        x1 = self.cv1(self.mp(x))
+        x2 = self.cv2(x)
+
+        # [B, C, H//2, W//2]
+        out = torch.cat([x1, x2], dim=1)
+
+        return out
+
+
+# ELANNet of YOLOv7
+class ELANNet(nn.Module):
+    """
+    CSPDarknet_53.
+    """
+    def __init__(self, depthwise=False):
+        super(ELANNet, self).__init__()
+
+        self.layer_1 = nn.Sequential(
+            Conv(3, 32, k=3, p=1, depthwise=depthwise),      
+            Conv(32, 64, k=3, p=1, s=2, depthwise=depthwise),
+            Conv(64, 64, k=3, p=1, depthwise=depthwise)                                                   # P1/2
+        )
+        self.layer_2 = nn.Sequential(   
+            Conv(64, 128, k=3, p=1, s=2, depthwise=depthwise),             
+            ELANBlock(in_dim=128, out_dim=256, expand_ratio=0.5, depthwise=depthwise)                     # P2/4
+        )
+        self.layer_3 = nn.Sequential(
+            DownSample(in_dim=256),             
+            ELANBlock(in_dim=256, out_dim=512, expand_ratio=0.5, depthwise=depthwise)                     # P3/8
+        )
+        self.layer_4 = nn.Sequential(
+            DownSample(in_dim=512),             
+            ELANBlock(in_dim=512, out_dim=1024, expand_ratio=0.5, depthwise=depthwise)                    # P4/16
+        )
+        self.layer_5 = nn.Sequential(
+            DownSample(in_dim=1024),             
+            ELANBlock(in_dim=1024, out_dim=1024, expand_ratio=0.25, depthwise=depthwise)                  # P5/32
+        )
+
+
+    def forward(self, x):
+        c1 = self.layer_1(x)
+        c2 = self.layer_2(c1)
+        c3 = self.layer_3(c2)
+        c4 = self.layer_4(c3)
+        c5 = self.layer_5(c4)
+
+        outputs = {
+            'layer2': c3,
+            'layer3': c4,
+            'layer4': c5
+        }
+        return outputs
+
+
+def build_elannet(pretrained=False):
+    # model
+    backbone = ELANNet()
+    feat_dims = [512, 1024, 1024]
+
+    # load weight
+    if pretrained:
+        print('Loading pretrained weight ...')
+        url = model_urls['elannet']
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url=url, map_location="cpu", check_hash=True)
+        # checkpoint state dict
+        checkpoint_state_dict = checkpoint.pop("model")
+        # model state dict
+        model_state_dict = backbone.state_dict()
+        # check
+        for k in list(checkpoint_state_dict.keys()):
+            if k in model_state_dict:
+                shape_model = tuple(model_state_dict[k].shape)
+                shape_checkpoint = tuple(checkpoint_state_dict[k].shape)
+                if shape_model != shape_checkpoint:
+                    checkpoint_state_dict.pop(k)
+            else:
+                checkpoint_state_dict.pop(k)
+                print(k)
+
+        backbone.load_state_dict(checkpoint_state_dict)
+
+    return backbone, feat_dims
+
+
+if __name__ == '__main__':
+    import time
+    model, feats = build_elannet(pretrained=False)
+    x = torch.randn(1, 3, 224, 224)
+    t0 = time.time()
+    outputs = model(x)
+    t1 = time.time()
+    print('Time: ', t1 - t0)
+    for k in outputs.keys():
+        print(outputs[k].shape)
---
+++ models/neck/elanpafpn.py
@@ -0,0 +1,175 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from ..basic.conv import Conv
+from ..basic.repconv import RepConv
+
+
+class ELANBlock(nn.Module):
+    """
+    ELAN BLock of YOLOv7's head
+    """
+    def __init__(self, in_dim, out_dim, expand_ratio=0.5, depthwise=False, act_type='silu', norm_type='BN'):
+        super(ELANBlock, self).__init__()
+        inter_dim = int(in_dim * expand_ratio)
+        inter_dim2 = int(inter_dim * expand_ratio)
+        self.cv1 = Conv(in_dim, inter_dim, k=1, act_type=act_type, norm_type=norm_type)
+        self.cv2 = Conv(in_dim, inter_dim, k=1, act_type=act_type, norm_type=norm_type)
+        self.cv3 = Conv(inter_dim, inter_dim2, k=3, p=1, depthwise=depthwise, act_type=act_type, norm_type=norm_type)
+        self.cv4 = Conv(inter_dim2, inter_dim2, k=3, p=1, depthwise=depthwise, act_type=act_type, norm_type=norm_type)
+        self.cv5 = Conv(inter_dim2, inter_dim2, k=3, p=1, depthwise=depthwise, act_type=act_type, norm_type=norm_type)
+        self.cv6 = Conv(inter_dim2, inter_dim2, k=3, p=1, depthwise=depthwise, act_type=act_type, norm_type=norm_type)
+
+        self.out = Conv(inter_dim*2+inter_dim2*4, out_dim, k=1)
+
+
+    def forward(self, x):
+        """
+        Input:
+            x: [B, C_in, H, W]
+        Output:
+            out: [B, C_out, H, W]
+        """
+        x1 = self.cv1(x)
+        x2 = self.cv2(x)
+        x3 = self.cv3(x2)
+        x4 = self.cv4(x3)
+        x5 = self.cv5(x4)
+        x6 = self.cv6(x5)
+
+        # [B, C_in, H, W] -> [B, C_out, H, W]
+        out = self.out(torch.cat([x1, x2, x3, x4, x5, x6], dim=1))
+
+        return out
+
+
+class DownSample(nn.Module):
+    def __init__(self, in_dim, depthwise=False, act_type='silu', norm_type='BN'):
+        super().__init__()
+        inter_dim = in_dim
+        self.mp = nn.MaxPool2d((2, 2), 2)
+        self.cv1 = Conv(in_dim, inter_dim, k=1, act_type=act_type, norm_type=norm_type)
+        self.cv2 = nn.Sequential(
+            Conv(in_dim, inter_dim, k=1, act_type=act_type, norm_type=norm_type),
+            Conv(inter_dim, inter_dim, k=3, p=1, s=2, depthwise=depthwise, act_type=act_type, norm_type=norm_type)
+        )
+
+    def forward(self, x):
+        """
+        Input:
+            x: [B, C, H, W]
+        Output:
+            out: [B, C, H//2, W//2]
+        """
+        # [B, C, H, W] -> [B, C//2, H//2, W//2]
+        x1 = self.cv1(self.mp(x))
+        x2 = self.cv2(x)
+
+        # [B, C, H//2, W//2]
+        out = torch.cat([x1, x2], dim=1)
+
+        return out
+
+
+# YOLOv7's PaFPN
+class YoloPaFPN(nn.Module):
+    def __init__(self, 
+                 in_dims=[256, 512, 512],
+                 out_dim=256,
+                 depthwise=False,
+                 norm_type='BN',
+                 act_type='silu'):
+        super(YoloPaFPN, self).__init__()
+        self.in_dims = in_dims
+        self.out_dim = out_dim
+        c3, c4, c5 = in_dims
+
+        # top dwon
+        ## P5 -> P4
+        self.cv1 = Conv(c5, 256, k=1, norm_type=norm_type, act_type=act_type)
+        self.cv2 = Conv(c4, 256, k=1, norm_type=norm_type, act_type=act_type)
+        self.head_elan_1 = ELANBlock(in_dim=256 + 256,
+                                     out_dim=256,
+                                     depthwise=depthwise,
+                                     norm_type=norm_type,
+                                     act_type=act_type)
+
+        # P4 -> P3
+        self.cv3 = Conv(256, 128, k=1, norm_type=norm_type, act_type=act_type)
+        self.cv4 = Conv(256, 128, k=1, norm_type=norm_type, act_type=act_type)
+        self.head_elan_2 = ELANBlock(in_dim=128 + 128,
+                                     out_dim=128,  # 128
+                                     depthwise=depthwise,
+                                     norm_type=norm_type,
+                                     act_type=act_type)
+
+        # bottom up
+        # P3 -> P4
+        self.mp1 = DownSample(128, depthwise=depthwise, act_type=act_type, norm_type=norm_type)
+        self.head_elan_3 = ELANBlock(in_dim=256 + 256,
+                                     out_dim=256,  # 256
+                                     depthwise=depthwise,
+                                     norm_type=norm_type,
+                                     act_type=act_type)
+
+        # P4 -> P5
+        self.mp2 = DownSample(256, depthwise=depthwise, act_type=act_type, norm_type=norm_type)
+        self.head_elan_4 = ELANBlock(in_dim=512 + 512,
+                                     out_dim=512,  # 512
+                                     depthwise=depthwise,
+                                     norm_type=norm_type,
+                                     act_type=act_type)
+
+        # RepConv
+        self.repconv_1 = RepConv(128, 256, k=3, s=1, p=1)
+        self.repconv_2 = RepConv(256, 512, k=3, s=1, p=1)
+        self.repconv_3 = RepConv(512, 1024, k=3, s=1, p=1)
+
+
+        # output proj layers
+        if self.out_dim is not None:
+            self.out_layers = nn.ModuleList([
+                Conv(in_dim, self.out_dim, k=1,
+                     norm_type=norm_type, act_type=act_type)
+                     for in_dim in in_dims
+                     ])
+
+    def forward(self, features):
+        c3, c4, c5 = features
+
+        # Top down
+        ## P5 -> P4
+        c6 = self.cv1(c5)
+        c7 = F.interpolate(c6, scale_factor=2.0)
+        c8 = torch.cat([c7, self.cv2(c4)], dim=1)
+        c9 = self.head_elan_1(c8)
+        ## P4 -> P3
+        c10 = self.cv3(c9)
+        c11 = F.interpolate(c10, scale_factor=2.0)
+        c12 = torch.cat([c11, self.cv4(c3)], dim=1)
+        c13 = self.head_elan_2(c12)
+
+        # Bottom up
+        # p3 -> P4
+        c14 = self.mp1(c13)
+        c15 = torch.cat([c14, c9], dim=1)
+        c16 = self.head_elan_3(c15)
+        # P4 -> P5
+        c17 = self.mp2(c16)
+        c18 = torch.cat([c17, c5], dim=1)
+        c19 = self.head_elan_4(c18)
+
+        # RepCpnv
+        c20 = self.repconv_1(c13)
+        c21 = self.repconv_2(c16)
+        c22 = self.repconv_3(c19)
+
+        out_feats = [c20, c21, c22] # [P3, P4, P5]
+        # output proj layers
+        if self.out_dim is not None:
+            out_feats_proj = []
+            for feat, layer in zip(out_feats, self.out_layers):
+                out_feats_proj.append(layer(feat))
+            return out_feats_proj
+
+        return out_feats
