ad72a628c0bfb42f2f6d08f12d37d6e386b3621b
yjh0410 <1394571815@qq.com>
2022-10-13 13:47:23 +0800

    train YOLO-Nano

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolo_free \
+        -v yolo_nano \
         --ema \
         --fp16 \
         --eval_epoch 10 \
