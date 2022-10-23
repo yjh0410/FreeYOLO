6ac7a76a833b6e7e708fc8929c8e1dfaf4d717a9
yjh0410 <1394571815@qq.com>
2022-10-23 17:32:44 +0800

    use SimOTA for yolo_sl

--- config/yolo_config.py
+++ config/yolo_config.py
@@ -365,7 +365,7 @@ yolo_config = {
         # training configuration
         'max_epoch': 300,
         'no_aug_epoch': 15,
-        'batch_size': 1,
+        'batch_size': 16,
         'accumulate': 1,
         'base_lr': 0.01 / 64.,
         'min_lr_ratio': 0.01,
