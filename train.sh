6e70a40f794487da9e68fff1df1aea7e36c6f117
yjh0410 <1394571815@qq.com>
2022-10-26 12:16:39 +0800

    fix a bug in mosaic

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolo_anchor \
+        -v yolo_free \
         --ema \
         --fp16 \
         --eval_epoch 10 \
-        --resume weights/coco/yolo_anchor/yolo_anchor_epoch_111_43.31.pth
+        --resume weights/coco/yolo_free/yolo_free_epoch_201_46.98.pth
