f63ad6271b35a878c8fff83c3ef44b663cc3e4b2
yjh0410 <1394571815@qq.com>
2022-10-27 19:20:48 +0800

    add ONNXRuntime

--- models/yolo_free/yolo_free.py
+++ models/yolo_free/yolo_free.py
@@ -174,9 +174,9 @@ class FreeYOLO(nn.Module):
         bboxes = bboxes.cpu().numpy()
 
         # nms
-        print(self.conf_thresh, self.nms_thresh, self.topk)
         scores, labels, bboxes = multiclass_nms(
             scores, labels, bboxes, self.nms_thresh, self.num_classes, False)
+        print(scores, len(scores))
 
         return bboxes, scores, labels
 
