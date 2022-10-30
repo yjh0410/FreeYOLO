5160583f6e63f2772fcf57a399720c097eabb3d4
yjh0410 <1394571815@qq.com>
2022-10-30 18:17:56 +0800

    update

--- models/yolo_free/yolo_free.py
+++ models/yolo_free/yolo_free.py
@@ -177,7 +177,7 @@ class FreeYOLO(nn.Module):
 
         # nms
         scores, labels, bboxes = multiclass_nms(
-            scores, labels, bboxes, self.nms_thresh, self.num_classes, True)
+            scores, labels, bboxes, self.nms_thresh, self.num_classes, False)
 
         return bboxes, scores, labels
 
