001ce465ee4ab0588cd9ef21644cdb304edd484e
yjh0410 <1394571815@qq.com>
2022-11-01 17:24:18 +0800

    update

--- config/yolo_config.py
+++ config/yolo_config.py
@@ -240,7 +240,7 @@ yolo_config = {
                         448, 480, 512, 544,
                         576, 608, 640],
         'mosaic_prob': 0.5,
-        'mixup_prob': 0.01,
+        'mixup_prob': 0.1,
         'format': 'RGB',
         'pixel_mean': [123.675, 116.28, 103.53],
         'pixel_std': [58.395, 57.12, 57.375],
