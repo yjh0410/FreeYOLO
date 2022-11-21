4e788d17e7051303981954129ec9d17a284cb5e7
yjh0410 <1394571815@qq.com>
2022-11-21 10:56:36 +0800

    modify prepcorss following YOLOv7

--- train.sh
+++ train.sh
@@ -3,9 +3,8 @@ python train.py \
         --cuda \
         -d widerface \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolo_free_tiny \
+        -v yolo_free_large \
         --ema \
         --fp16 \
         --eval_epoch 10 \
-        --coco_pretrained weights/coco/yolo_free_tiny/yolo_free_tiny_31.1.pth \
         # --resume weights/coco/yolo_free_large/yolo_free_large_47.1.pth \
