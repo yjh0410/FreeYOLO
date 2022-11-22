b4001eef678a456fce36e546f12bd45df9bdea37
yjh0410 <1394571815@qq.com>
2022-11-22 09:30:01 +0800

    add random_flip

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,10 +1,10 @@
 # 2 GPUs
 # Attention, the following batch size is on single GPU, not all GPUs.
-python -m torch.distributed.run --nproc_per_node=3 train.py \
+python -m torch.distributed.run --nproc_per_node=4 train.py \
                                                     --cuda \
                                                     -dist \
                                                     -d coco \
-                                                    --root /data/datasets/COCO/ \
+                                                    --root /data/datasets/ \
                                                     -v yolo_free_huge \
                                                     --ema \
                                                     --fp16 \
