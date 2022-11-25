13c3410fd9a8f44f95bb750082385c434e25d5e7
yjh0410 <1394571815@qq.com>
2022-11-25 09:49:09 +0800

    fix a bug about mosaic & mixup

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,6 +1,6 @@
 # 2 GPUs
 # Attention, the following batch size is on single GPU, not all GPUs.
-python -m torch.distributed.run --nproc_per_node=4 train.py \
+python -m torch.distributed.run --nproc_per_node=2 train.py \
                                                     --cuda \
                                                     -dist \
                                                     -d coco \
